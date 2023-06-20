77eea92a027cef8da382381496db5cd2677f2710
YurijCroz <valetkrest@gmail.com>
2023-06-20 21:19:19 +0300

    fix node:https

--- server/server.js
+++ server/server.js
@@ -1,17 +1,11 @@
 "use strict";
-const https = require("https");
-// const fs = require("fs");
+const https = require("node:https");
 const app = require("./app.js");
 
 const PORT = 3000;
 
-// const options = {
-//   key: fs.readFileSync("../private_key.pem"),
-//   cert: fs.readFileSync("../certificate.pem"),
-// };
+const server = https.createServer(app);
 
-const server = https.createServer(/* options, */ app);
-
-server.listen(PORT, () =>
-  console.log(`Example app listening on port >>> ${PORT}! <<<`)
-);
+server.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
