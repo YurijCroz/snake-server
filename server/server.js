97788753d1b52387d83161e9beb9f80e28b5387e
YurijCroz <valetkrest@gmail.com>
2023-06-21 12:36:45 +0300

    test server only express

--- server/server.js
+++ server/server.js
@@ -1,21 +1,24 @@
 "use strict";
-const https = require("https");
+// const https = require("https");
 const app = require("./app.js");
-const fs = require("fs");
-const path = require("path");
+// const fs = require("fs");
+// const path = require("path");
 
-const keyPem = path.resolve(__dirname, "../key.pem");
-const certPem = path.resolve(__dirname, "../cert.pem");
+// const keyPem = path.resolve(__dirname, "../key.pem");
+// const certPem = path.resolve(__dirname, "../cert.pem");
 
-const options = {
-  key: fs.readFileSync(keyPem),
-  cert: fs.readFileSync(certPem),
-};
+// const options = {
+//   key: fs.readFileSync(keyPem),
+//   cert: fs.readFileSync(certPem),
+// };
 
 const PORT = 443;
 
-const server = https.createServer(options, app);
+// const server = https.createServer(options, app);
 
-server.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+server.keepAliveTimeout = 120 * 1000;
+server.headersTimeout = 120 * 1000;
