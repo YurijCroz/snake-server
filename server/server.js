12f270601766ad5d7c3f18d7f3c243f280d9c458
Yurii Croz <valetkrest@gmail.com>
2023-06-29 14:29:18 +0300

    test

--- server/server.js
+++ server/server.js
@@ -1,20 +1,7 @@
 "use strict";
-// const https = require("https");
 const app = require("./app.js");
-// const fs = require("fs");
-// const path = require("path");
-
-// const keyPem = path.resolve(__dirname, "../key.pem");
-// const certPem = path.resolve(__dirname, "../cert.pem");
-
-// const options = {
-//   key: fs.readFileSync(keyPem),
-//   cert: fs.readFileSync(certPem),
-// };
 
 const PORT = 443;
-
-// const server = https.createServer(options, app);
 
 const server = app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
